00059ec4723167da4a74491bce1e620e161b9263
fajrCode <fajri42vandi@gmail.com>
2024-05-30 00:07:20 +0700

    set seader with route

--- app/server/routes/warga/warga_controller.py
+++ app/server/routes/warga/warga_controller.py
@@ -1,23 +1,50 @@
-from flask import request
+import pandas as pd
+# from flask import request
 from app.server import db
 from app.server.model.warga import Warga
 from app.server.helper import response
 from app.server.helper.formating import dataWargaAll
 
-import numpy as np
-import pandas as pd
-
 
 def allData():
     try:
         data = Warga.query.all()
-
         if not data:
-            return response.badReq([], "Data not Found")
+            return "empty"
 
-        result = dataWargaAll(data)
+        return dataWargaAll(data)
+    except Exception as e:
+        print(e)
+        return False
+    
+def seeder(file_path):
+    try:
+        # Membaca data dari file Excel
+        data = pd.read_excel(file_path)
 
-        return result
+        # Iterasi melalui baris-baris di DataFrame
+        for index, row in data.iterrows():
+            # Konversi penghasilan dari string ke integer
+            # penghasilan = int(str(row['penghasilan']).replace('.', ''))
+
+            # Buat instance model dan tambahkan ke sesi
+            record = Warga(
+                nik=row['nik'],
+                nama=row['nama'],
+                alamat=row['alamat'],
+                no_rt=row['no_rt'],
+                pekerjaan=row['pekerjaan'],
+                penghasilan=row['penghasilan'],
+                tanggungan=row['tanggungan'],
+                kondisi_rumah=row['kondisi_rumah'],
+                status_rumah=row['status_rumah'],
+                jenis=row['jenis']
+            )
+            db.session.add(record)
+            
+        # Komit perubahan ke database
+        db.session.commit()
+        return True
     except Exception as e:
         print(e)
         return False
--- app/server/routes/warga/warga_router.py
+++ app/server/routes/warga/warga_router.py
@@ -1,17 +1,26 @@
+import os
 from flask import Blueprint, request, render_template, redirect
-import pandas as pd
+# import pandas as pd
 from app.server.routes.warga import warga_controller
 
 Warga = Blueprint('warga', __name__)
 
 @Warga.route("/")
-
 def warga():
-    df = pd.read_excel('dataset_nonlabel_test.xlsx')
-    data = df.to_dict(orient='records')
+    # df = pd.read_excel('dataset_nonlabel_test.xlsx')
+    # data = df.to_dict(orient='records')
     
     result = warga_controller.allData()
     if not result:
         return render_template("pages/error/500.jinja", name="Admin")
-    
     return render_template("pages/warga/list.jinja", name="Admin", data=result)
+
+@Warga.route("/seeder")
+def seeder():
+    # Menentukan path file Excel relatif terhadap root project
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../../dataset.xlsx')
+    seed = warga_controller.seeder(file_path)
+    if not seed:
+        return render_template("pages/error/500.jinja", name="Admin")
+    
+    return redirect("/warga")
