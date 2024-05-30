b1dae8ed7ef4d001b313e6cd56a293b2d08372a4
fajrCode <fajri42vandi@gmail.com>
2024-05-30 10:24:10 +0700

    feat: warga | split tabel by jenis

--- app/server/routes/warga/warga_router.py
+++ app/server/routes/warga/warga_router.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, request, render_template, redirect
+from flask import Blueprint, render_template, redirect
 # import pandas as pd
 from app.server.routes.warga import warga_controller
 
@@ -14,6 +14,34 @@ def warga():
     if not result:
         return render_template("pages/error/500.jinja", name="Admin")
     return render_template("pages/warga/list.jinja", name="Admin", data=result)
+
+@Warga.route("/miskin-extreme")
+def miskinextreme():
+    result = warga_controller.allDataByJenis("miskin extreme")
+    if not result:
+        return render_template("pages/error/500.jinja", name="Admin")
+    return render_template("pages/warga/miskin_extreme.jinja", name="Admin", data=result)
+
+@Warga.route("/pkh")
+def pkh():
+    result = warga_controller.allDataByJenis("pkh")
+    if not result:
+        return render_template("pages/error/500.jinja", name="Admin")
+    return render_template("pages/warga/pkh.jinja", name="Admin", data=result)
+
+@Warga.route("/cbp")
+def cbp():
+    result = warga_controller.allDataByJenis("cbp")
+    if not result:
+        return render_template("pages/error/500.jinja", name="Admin")
+    return render_template("pages/warga/cbp.jinja", name="Admin", data=result)
+
+@Warga.route("/bukan-penerima")
+def bukan_penerima():
+    result = warga_controller.allDataByJenis("tidak layak")
+    if not result:
+        return render_template("pages/error/500.jinja", name="Admin")
+    return render_template("pages/warga/bukan_penerima.jinja", name="Admin", data=result)
 
 @Warga.route("/seeder")
 def seeder():
